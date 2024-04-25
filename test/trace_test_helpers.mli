e4c37ff8429b8ec7f30696977b02468253950160
public-release <as-public-release@janestreet.com>
2024-04-25 19:19:21 +0100

    v0.17~preview.129.17+77

--- test/trace_test_helpers.mli
+++
@@ -1,9 +0,0 @@
-open! Core
-
-val trace_to_buf
-  :  ?num_temp_strs:int
-  -> (Tracing_zero.Writer.t -> unit)
-  -> (read_write, 'a) Iobuf.t
-
-val print_records_until_error : Tracing.Parser.t -> unit
-val print_records_until : Tracing.Parser.t -> Tracing.Parser.Parse_error.t
